002dec91d7d02d44448db9166f5e2307d5667b99
wesleylo <weslo404@gmail.com>
2020-09-19 23:21:59 -0400

    Add printlist

--- PyBank/main.py
+++ PyBank/main.py
@@ -27,11 +27,26 @@ with open(csvpath) as csvfile:
             toploss[0] = row[0]
             toploss[1] = current 
 
+printlist = [
+    "Financial Analysis",
+    "----------------------------",
+    "Total Months: " + str(numrows),
+    "Total: $" + str(total),
+    "Average Change: $" + str(round(float(total)/float(numrows), 2)), # Calculate float avg and round to 2 ndigits
+    "Greatest Increase in Profits: " + topprofit[0] + " ($" + str(topprofit[1]) + ")",
+    "Greatest Decrease in Profits: " + toploss[0] + " ($" + str(toploss[1]) + ")"
+]
+print(printlist)
+for row in printlist:
+    print(row)
 
-print("Financial Analysis")
-print("----------------------------")
-print("Total Months: " + str(numrows))
-print("Total: $" + str(total))
-print("Average Change: $" + str(round(float(total)/float(numrows), 2))) # Calculate float avg and round to 2 ndigits
-print("Greatest Increase in Profits: " + topprofit[0] + " ($" + str(topprofit[1]) + ")")
-print("Greatest Decrease in Profits: " + toploss[0] + " ($" + str(toploss[1]) + ")")
+# print("Financial Analysis")
+# print("----------------------------")
+# print("Total Months: " + str(numrows))
+# print("Total: $" + str(total))
+# print("Average Change: $" + str(round(float(total)/float(numrows), 2))) # Calculate float avg and round to 2 ndigits
+# print("Greatest Increase in Profits: " + topprofit[0] + " ($" + str(topprofit[1]) + ")")
+# print("Greatest Decrease in Profits: " + toploss[0] + " ($" + str(toploss[1]) + ")")
+
+
+f = open(os.path.join('analysis', "anaysis.txt"), "w+")
