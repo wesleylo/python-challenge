3174fa11c8a58da1c855b9ce03ef3158a82863c1
wesleylo <weslo404@gmail.com>
2020-09-20 00:24:05 -0400

    Add comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,26 +14,11 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         currentcandidate = row[2]
         numrows += 1 # Iterate numrows
-        if not currentcandidate in candidates: 
+        if not currentcandidate in candidates: # currentcandidate does not appear in candidates
             candidates.append(currentcandidate)
             votecounts.append(0)
 
         votecounts[candidates.index(currentcandidate)] += 1 # Find index of current candidate and iterate corresponding votecount
-
-
-
-# print("Election Results")
-# print("-------------------------")
-# print("Total Votes: " + str(numrows))
-# print("-------------------------")
-# for candidate in candidates:
-#     numvotes = votecounts[candidates.index(candidate)]
-#     print(candidate + ": " + str(round(float(numvotes)/float(numrows)*100, 3)) + "% (" + str(numvotes) + ")") # Calculate vote percentage
-# print("-------------------------")
-# print("Winner: " + str(candidates[votecounts.index(max(votecounts))])) # Display candidate with matching index to the max votecount
-# print("-------------------------")
-
-
 
 printlist = [
     "Election Results",
